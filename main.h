2f72f6aa2123162fc8d1f1b2eab282c7866a2bd1
NoureddineLz <lazri.noureddine.contact@gmail.com>
2023-08-19 11:50:32 -0700

    deleted

--- main.h
+++ main.h
@@ -82,6 +82,7 @@ int handle_write_char(char c, char buffer[],
     int flags, int width, int precision, int size);
 int write_number(int is_positive, int ind, char buffer[],
     int flags, int width, int precision, int size);
+int vi_eid(va_list types, ...);
 int write_num(int ind, char bff[], int flags, int width, int precision,
     int length, char padd, char extra_c);
 int write_pointer(char buffer[], int ind, int length,
